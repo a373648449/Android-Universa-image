a700890fc64cdb305174ee86f10544197ae0cefc
guoqiang <373648449@qq.com>
2015-11-06 10:38:39 +0800

    aaa

---
+++ library/src/main/java/com/nostra13/universalimageloader/utils/ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.nostra13.universalimageloader.utils;
+
+import android.app.Application;
+
+
+/**
+ * Created by guoqiang on 2015/10/30.
+ */
+public class ExampleApplication extends Application {
+
+
+
+}
